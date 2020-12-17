c0b45b73bf588dfe1e2de1db4a72ca0b93aaa14d
Jodecir Neto <jodecirneto@gmail.com>
2020-12-17 15:42:36 -0300

    ship movement

--- js/script.js
+++ js/script.js
@@ -1,2 +1,39 @@
 const yourShip = document.querySelector('.player-shooter');
 const playArea = document.querySelector('#main-play-game');
+
+function flyShip(event) {
+  if(event.key === 'ArrowUp') {
+    event.preventDefault();
+    moveUp();
+  } else if(event.key === 'ArrowDown') {
+      event.preventDefault();
+      moveDown();
+  } else if(event.key === "") {
+      event.preventDefault();
+      fireLaser();
+  }
+}
+
+function moveUp() {
+  let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+  if(topPosition === "0px") {
+    return
+  } else {
+      let position = parseInt(topPosition);
+      position -= 50;
+      yourShip.style.top = `${position}px`;
+  }
+}
+
+function moveDown() {
+  let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+  if(topPosition === "500px") {
+    return
+  } else {
+      let position = parseInt(topPosition);
+      position += 50;
+      yourShip.style.top = `${position}px`;
+  }
+}
+
+window.addEventListener('keydown', flyShip);
