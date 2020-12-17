3519f514f1bbd90afea3b4d1d9e9ab9e66c2fcfb
Jodecir Neto <jodecirneto@gmail.com>
2020-12-17 16:34:25 -0300

    shoot

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const yourShip = document.querySelector('.player-shooter');
-const playArea = document.querySelector('#main-play-game');
+const playArea = document.querySelector('#main-play-area');
 
 function flyShip(event) {
   if(event.key === 'ArrowUp') {
@@ -8,7 +8,7 @@ function flyShip(event) {
   } else if(event.key === 'ArrowDown') {
       event.preventDefault();
       moveDown();
-  } else if(event.key === "") {
+  } else if(event.key === " ") {
       event.preventDefault();
       fireLaser();
   }
@@ -27,13 +27,42 @@ function moveUp() {
 
 function moveDown() {
   let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
-  if(topPosition === "500px") {
+  if(topPosition === "550px") {
     return
   } else {
       let position = parseInt(topPosition);
       position += 50;
       yourShip.style.top = `${position}px`;
   }
+}
+
+function fireLaser() {
+  let laser = createLaserElement();
+  playArea.appendChild(laser);
+  moveLaser(laser);
+}
+
+function createLaserElement() {
+  let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+  let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+  let newLaser = document.createElement('img');
+  newLaser.src = '../img/shoot.png';
+  newLaser.classList.add('laser');
+  newLaser.style.left = `${xPosition}px`;
+  newLaser.style.top = `${yPosition - 10}px`;
+  return newLaser;
+}
+
+function moveLaser(laser) {
+  let laserInterval = setInterval(() => {
+    let xPosition = parseInt(laser.style.left);
+    
+    if (xPosition === 340) {
+      laser.remove();
+    } else {
+        laser.style.left = `${xPosition + 8}px`;
+    }
+  }, 10);
 }
 
 window.addEventListener('keydown', flyShip);
