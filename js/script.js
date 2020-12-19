2ff0cec60a00d92893fc2782ca22da2977d964c2
Jodecir Neto <jodecirneto@gmail.com>
2020-12-19 13:56:07 -0300

    better collision

--- js/script.js
+++ js/script.js
@@ -110,13 +110,13 @@ function moveAlien(alien){
 function checkLaserCollision(laser, alien) {
   let laserTop = parseInt(laser.style.top);
   let laserLeft = parseInt(laser.style.left);
-  let laserBottom = laserTop - 20;
+  let laserCenter = laserTop + 15;
   let alienTop = parseInt(alien.style.top);
   let alienLeft = parseInt(alien.style.left);
   let alienBottom = alienTop - 30;
   
   if (laserLeft != 340 && laserLeft + 40 >= alienLeft) {
-    if (laserTop <= alienTop && laserTop >= alienBottom) {
+    if (laserCenter <= alienTop && laserCenter >= alienBottom) {
       return true
     } else {
         return false;
